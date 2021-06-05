e8900dee1a350cf0ca98dcdd935f1ca08bcd3379
TheNexusAvenger <13441476+TheNexusAvenger@users.noreply.github.com>
2021-06-04 23:15:02 -0400

    Add additional tools and animations to loading.

--- src/ReplicatedStorage/Data/Assets.lua
+++ src/ReplicatedStorage/Data/Assets.lua
@@ -26,7 +26,7 @@ local function Mesh(Name,URL)
     SpecialMesh.Name = Name
     SpecialMesh.MeshType = Enum.MeshType.FileMesh
     SpecialMesh.MeshId = URL
-    return Mesh
+    return SpecialMesh
 end
 
 --[[
@@ -37,6 +37,16 @@ local function Sound(Name,URL)
     Audio.Name = Name
     Audio.SoundId = URL
     return Audio
+end
+
+--[[
+Prepares an asset that is an Animation.
+--]]
+local function Animation(Name,URL)
+    local AnimationTrack = Instance.new("Animation")
+    AnimationTrack.Name = Name
+    AnimationTrack.AnimationId = URL
+    return AnimationTrack
 end
 
 
@@ -63,7 +73,31 @@ local Assets = {
     --UI
     Sound("Timer tick sound","rbxassetid://156286438"),
     Image("Coin","rbxassetid://121148238"),
-    Image("Rank Icons","rbxassetid://6866145490")
+    Image("Rank Icons","rbxassetid://6866145490"),
+
+    --Other weapons (not covered in weapon icons)
+    Mesh("Rocket Launcher mesh","rbxassetid://94690054"),
+    Image("Rocket Launcher texture","rbxassetid://94689966"),
+    Image("Red bomb texture","rbxassetid://94691735"),
+
+    --Weapon animations
+    Animation("R15 Bomb Hold Animation","rbxassetid://2960980426"),
+    Animation("R15 Bomb Throw Animation","rbxassetid://2960980426"),
+    Animation("R15 Broom Idle Animation","rbxassetid://2960982045"),
+    Animation("R15 Broom Whack Animation","rbxassetid://2960982598"),
+    Animation("R15 Reflector Activate Animation","rbxassetid://2960983050"),
+    Animation("R15 Rocket Launcher Fire Animation","rbxassetid://2960984492"),
+    Animation("R15 Slingshot Equip Animation","rbxassetid://2960984865"),
+    Animation("R15 Slingshot Fire Animation","rbxassetid://2960985364"),
+    Animation("R15 Superball Equip Animation","rbxassetid://2960986220"),
+    Animation("R15 Superball Idle Animation","rbxassetid://2960986577"),
+    Animation("R15 Superball Throw Animation","rbxassetid://2960986912"),
+    Animation("R15 Sword Equip Animation","rbxassetid://2960987338"),
+    Animation("R15 Sword Unequip Animation","rbxassetid://2960987857"),
+    Animation("R15 Sword Idle Animation","rbxassetid://2960988663"),
+    Animation("R15 Sword Slash Animation","rbxassetid://2960989171"),
+    Animation("R15 Sword Thrust Animation","rbxassetid://2960989618"),
+    Animation("R15 Sword Overhead Animation","rbxassetid://2960990006"),
 }
 
 --Add the maps.
